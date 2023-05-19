b9485318668754194f0a17ab11d367afd22d5d7c
Tohrwarneth <Frederic.n.Schneider@web.de>
2023-05-19 15:30:59 +0200

    Finished GUI

--- src/ui/gui_clock.py
+++ src/ui/gui_clock.py
@@ -1,52 +1,67 @@
 import pygame
-from simpy import Environment
-
 from src.utils import Conf, Clock
 
 
 class GuiClock:
+    """
+    GUI Representation of the Clock and simulation speed
+    """
+    flashTick: float
+    remainingFlashTick: float
+    isVisible: bool  # Visible = True, Hidden = False
+    timeText: str
+    position: tuple[float, float]
 
     def __init__(self):
-        self.flashTick: float = 0.6
-        self.remainingFlashTick: float = 0
-        self.flashState: bool = True  # Visible = True, Hidden = False
-        self.time_text: str = "00:00"
-        self.position: tuple[float, float] = (0, 0)
+        self.flashTick = 0.6
+        self.remainingFlashTick = 0
+        self.isVisible = True
+        self.timeText = "00:00"
+        self.position = (0, 0)
+
         self.update_screen_scale()
 
     def update(self) -> None:
+        """
+        Update non referenced variable
+        :return: None
+        """
         sec = Clock.tact
         m = int(sec % 60)
         h = int(sec / 60)
-        self.time_text = f"{h:02d}:{m:02d}"
+        self.timeText = f"{h:02d}:{m:02d}"
 
     def render(self, game_display: pygame.Surface) -> None:
+        """
+        Rendering GUI
+        :param game_display: master surface
+        :return: None
+        """
         self.update()
 
-        clock_surface: pygame.Surface = Conf.fontLarge.render(self.time_text, True, "black")
+        clock_surface: pygame.Surface = Conf.fontLarge.render(self.timeText, True, "black")
 
         clock_rect: pygame.Rect = clock_surface.get_rect()
         clock_rect.center = self.position
-        if self.flashState:
+        if self.isVisible:
             game_display.blit(clock_surface, clock_rect)
 
         if Clock.pause:
             self.remainingFlashTick -= Clock.deltaTime
             if self.remainingFlashTick <= 0:
                 self.remainingFlashTick = self.flashTick
-                self.flashState = not self.flashState
+                self.isVisible = not self.isVisible
 
-            pause_surface: pygame.Surface = Conf.fontLarge.render("Pause", True, "black")
+            pause_surface: pygame.Surface = Conf.fontLarge.render("Pause", True, "#333333")
 
             pause_rect: pygame.Rect = pause_surface.get_rect()
             width, height = self.position
-            height_offset = Conf.screenSize[1] / 15
+            height_offset: float = Conf.screenSize[1] / 15
             pause_rect.center = (width, height + height_offset)
-            if not self.flashState:
-                game_display.blit(pause_surface, pause_rect)
+            game_display.blit(pause_surface, pause_rect)
         else:
             self.remainingFlashTick = 0.0
-            self.flashState = True
+            self.isVisible = True
 
         status_text: str = f"Geschwindigkeit (+/-): {Clock.speedScale if not Clock.pause else Clock.speedPrePaused}"
         scale_surface = Conf.fontLarge.render(status_text, True, "black")
@@ -54,7 +69,11 @@ class GuiClock:
         game_display.blit(scale_surface, (5, 5))
 
     def update_screen_scale(self):
-        screen_size = Conf.screenSize
-        width = screen_size[0] / 1.863
-        height = screen_size[1] / 12.5
+        """
+        Update position and font according to the new resolution
+        :return: None
+        """
+        screen_size: tuple[float, float] = Conf.screenSize
+        width: float = screen_size[0] / 1.863
+        height: float = screen_size[1] / 12.5
         self.position = (width, height)
--- src/ui/gui_floor.py
+++ src/ui/gui_floor.py
@@ -6,25 +6,41 @@ from src.utils import Conf
 
 
 class GuiFloor:
+    """
+    GUI Representation of up or down calls of each floor
+    """
+    call: tuple[list[list[Person]], list[list[Person]]]
+    position: tuple[float, float]
 
     def __init__(self, call_up: list[list[Person]], call_down: list[list[Person]]):
-        self.call: tuple[list[list[Person]], list[list[Person]]] = (call_up, call_down)
-        self.position: tuple[float, float] = (0, 0)
+        self.call = (call_up, call_down)
+        self.position = (0, 0)
 
         self.update_screen_scale()
 
     def render(self, game_display: pygame.Surface) -> None:
+        """
+        Rendering GUI
+        :param game_display: master surface
+        :return: None
+        """
         self.draw_floor(game_display, Direction.UP)
         self.draw_floor(game_display, Direction.DOWN)
 
-    def draw_floor(self, game_display, direction: Direction):
+    def draw_floor(self, game_display, direction: Direction) -> None:
+        """
+        Rendering up/down call of each floor
+        :param game_display: master surface
+        :param direction: up or down call
+        :return: None
+        """
         (sw, sh) = Conf.screenScale
         for index, floor in enumerate(reversed(self.call[direction.value])):
             if len(floor) > 0:
                 if direction.value == 0:
-                    image_arrow = pygame.image.load('images/Pfeil_hoch.png')
+                    image_arrow: pygame.Surface = pygame.image.load('images/Pfeil_hoch.png')
                 else:
-                    image_arrow = pygame.image.load('images/Pfeil_runter.png')
+                    image_arrow: pygame.Surface = pygame.image.load('images/Pfeil_runter.png')
                 image_arrow.convert()
 
                 image_arrow = \
@@ -34,33 +50,38 @@ class GuiFloor:
 
                 width, height = self.position
                 if direction == 1:
-                    offset_width = width / 10
+                    offset_width: float = width / 10
                 else:
-                    offset_width = 0
-                offset_height = height / 3.15 * index
+                    offset_width: float = 0
+                offset_height: float = height / 3.15 * index
 
                 arrow_rect: pygame.Rect = image_arrow.get_rect()
                 arrow_rect.center = (offset_width + width, offset_height + height)
                 game_display.blit(image_arrow, arrow_rect)
 
             if not (index == 14 and direction == direction.DOWN):
+                #  If not first floor and face downwards, display number of calls of each direction
                 text_surface: pygame.Surface = Conf.fontSmall. \
                     render(f"{len(floor):01d}", True, "black")
                 text_rect: pygame.Rect = text_surface.get_rect()
 
                 width, height = self.position
-                offset_width = Conf.screenSize[0] / 30
+                offset_width: float = Conf.screenSize[0] / 30
                 if direction == 1:
                     offset_width += width / 5
                 else:
                     offset_width += width / 30
-                offset_height = height / 3.15 * index
+                offset_height: float = height / 3.15 * index
 
                 text_rect.center = (offset_width + width, offset_height + height)
                 game_display.blit(text_surface, text_rect)
 
     def update_screen_scale(self):
-        screen_size = Conf.screenSize
-        width = screen_size[0] / 12
-        height = screen_size[1] / 6.5
+        """
+        Update position and font according to the new resolution
+        :return: None
+        """
+        screen_size: tuple[float, float] = Conf.screenSize
+        width: float = screen_size[0] / 12
+        height: float = screen_size[1] / 6.5
         self.position = (width, height)
