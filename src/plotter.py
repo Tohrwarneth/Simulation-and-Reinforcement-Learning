9a57a17375764abc95ebe4c3321a0aa8566c1d96
Tohrwarneth <Frederic.n.Schneider@web.de>
2023-07-05 08:15:37 +0200

    draw data in plotter.py

--- src/plotter.py
+++ src/plotter.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utils import Logger, Conf
+
 
 def plot_learning_curve(x, scores, figure_file, figure_name='Running average of previous 100 scores'):
     if isinstance(scores, list):
@@ -15,3 +17,89 @@ def plot_learning_curve(x, scores, figure_file, figure_name='Running average of 
         figure_file = 'images/'
         figure_file += figure_name.replace(' ', '_')
     plt.savefig(figure_file)
+
+
+def draw_data(simulation) -> None:
+    """
+    Drawing data to plots
+    :return: None
+    """
+    # gamma
+    #
+    fig, axs = plt.subplots(layout='constrained')
+    plt.hist(simulation.personManager.scheduleTimes, bins=24 * 60, density=True)
+
+    plt.xlabel('Zeit [Minuten]')
+    plt.ylabel('Dichte')
+    min_to_hour = lambda x: np.divide(x, 60)
+    secax1 = axs.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
+    secax1.set_xlabel('Zeit [Stunden]')
+
+    plt.title('Gamma-Verteilung')
+
+    Logger.log('Gamma-Verteilung')
+    if Conf.showPlots:
+        plt.show()
+
+    # floors
+    #
+    floors = list()
+    for f in simulation.personManager.homeFloors:
+        floors.append(int(f) + 1)
+
+    plt.hist(floors, bins=[i for i in range(1, Conf.maxFloor + 1)], density=True, alpha=0.8)
+
+    plt.xlabel('Etagen')
+    plt.ylabel('Dichte')
+
+    plt.title('Stockwerk-Verteilung')
+
+    Logger.log(plot_name='Etagen-Verteilung')
+    if Conf.showPlots:
+        plt.show()
+
+    fig: plt.Figure = plt.figure()
+    ax_motion: plt.Axes = fig.add_subplot(211)
+    ax_avg: plt.Axes = fig.add_subplot(212)
+    ax_motion.plot([i for i in range(24 * 60)], simulation.personManager.numberInMotion)
+    ax_motion.set_xlabel('Zeit [Minuten]')
+    ax_motion.set_ylabel('Personen')
+    secax0 = ax_motion.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
+    secax0.set_xlabel('Zeit [Stunden]')
+    ax_motion.title.set_text('Reisende Personen')
+
+    ax_avg.plot([i for i in range(24 * 60)], simulation.avgWaitingTime)
+    ax_avg.set_xlabel('Zeit [Minuten]')
+    secax1 = ax_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
+    secax1.set_xlabel('Zeit [Stunden]')
+    ax_avg.set_ylabel('Wartezeit [Minuten]')
+    fig.suptitle("Durchschnittliche Wartezeit")
+
+    fig.tight_layout()
+
+    Logger.log(plot_name='Durchschnittliche-Wartezeit')
+    if Conf.showPlots:
+        fig.show()
+
+    # average waiting time
+    #
+    fig: plt.Figure = plt.figure()
+    ax_avg: plt.Axes = fig.add_subplot(211)
+    ax_final_avg: plt.Axes = fig.add_subplot(212)
+
+    ax_avg.plot([i for i in range(24 * 60)], simulation.avgWaitingTime)
+    ax_avg.set_xlabel('Zeit [Minuten]')
+    secax1 = ax_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
+    secax1.set_xlabel('Zeit [Stunden]')
+    ax_avg.set_ylabel('Wartezeit [Minuten]')
+    fig.suptitle("Durchschnittliche Wartezeit")
+
+    ax_final_avg.plot([i for i in range(24 * 60)], simulation.finalAvgWaitingTime)
+    ax_final_avg.set_xlabel('Zeit [Minuten]')
+    secax2 = ax_final_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
+    secax2.set_xlabel('Zeit [Stunden]')
+    ax_final_avg.set_ylabel('Wartezeit [Minuten]')
+    ax_final_avg.title.set_text('Finale Durchschnittliche Wartezeit')
+    fig.tight_layout()
+    if Conf.showPlots:
+        fig.show()
--- src/simulation.py
+++ src/simulation.py
@@ -5,6 +5,7 @@ import torch
 from matplotlib import pyplot as plt
 
 import enums
+import plotter
 from logic.person import Person
 from new_re_learner import trainer, agent
 from re_learner import NetCoder
@@ -180,7 +181,7 @@ class Simulation:
             print(f"Average Waiting Time: {data['avgWaitingTime']:.2f} | "
                   f"Remaining Persons: {data['remaining']}")
         if Conf.generatesPlots:
-            self.draw_data()
+            plotter.draw_data(self)
 
     def end_of_day_log(self) -> dict:
         """
@@ -217,91 +218,6 @@ class Simulation:
             self.personManager.get_remaining_people(), \
             self.callUp, self.callDown, \
             tuple(elevators)
-
-    def draw_data(self) -> None:
-        """
-        Drawing data to plots
-        :return: None
-        """
-        # gamma
-        #
-        fig, axs = plt.subplots(layout='constrained')
-        plt.hist(self.personManager.scheduleTimes, bins=24 * 60, density=True)
-
-        plt.xlabel('Zeit [Minuten]')
-        plt.ylabel('Dichte')
-        min_to_hour = lambda x: np.divide(x, 60)
-        secax1 = axs.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
-        secax1.set_xlabel('Zeit [Stunden]')
-
-        plt.title('Gamma-Verteilung')
-
-        Logger.log('Gamma-Verteilung')
-        if Conf.showPlots:
-            plt.show()
-
-        # floors
-        #
-        floors = list()
-        for f in self.personManager.homeFloors:
-            floors.append(int(f) + 1)
-
-        plt.hist(floors, bins=[i for i in range(1, Conf.maxFloor + 1)], density=True, alpha=0.8)
-
-        plt.xlabel('Etagen')
-        plt.ylabel('Dichte')
-
-        plt.title('Stockwerk-Verteilung')
-
-        Logger.log(plot_name='Etagen-Verteilung')
-        if Conf.showPlots:
-            plt.show()
-
-        fig: plt.Figure = plt.figure()
-        ax_motion: plt.Axes = fig.add_subplot(211)
-        ax_avg: plt.Axes = fig.add_subplot(212)
-        ax_motion.plot([i for i in range(24 * 60)], self.personManager.numberInMotion)
-        ax_motion.set_xlabel('Zeit [Minuten]')
-        ax_motion.set_ylabel('Personen')
-        secax0 = ax_motion.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
-        secax0.set_xlabel('Zeit [Stunden]')
-        ax_motion.title.set_text('Reisende Personen')
-
-        ax_avg.plot([i for i in range(24 * 60)], self.avgWaitingTime)
-        ax_avg.set_xlabel('Zeit [Minuten]')
-        secax1 = ax_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
-        secax1.set_xlabel('Zeit [Stunden]')
-        ax_avg.set_ylabel('Wartezeit [Minuten]')
-        fig.suptitle("Durchschnittliche Wartezeit")
-
-        fig.tight_layout()
-
-        Logger.log(plot_name='Durchschnittliche-Wartezeit')
-        if Conf.showPlots:
-            fig.show()
-
-        # average waiting time
-        #
-        fig: plt.Figure = plt.figure()
-        ax_avg: plt.Axes = fig.add_subplot(211)
-        ax_final_avg: plt.Axes = fig.add_subplot(212)
-
-        ax_avg.plot([i for i in range(24 * 60)], self.avgWaitingTime)
-        ax_avg.set_xlabel('Zeit [Minuten]')
-        secax1 = ax_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
-        secax1.set_xlabel('Zeit [Stunden]')
-        ax_avg.set_ylabel('Wartezeit [Minuten]')
-        fig.suptitle("Durchschnittliche Wartezeit")
-
-        ax_final_avg.plot([i for i in range(24 * 60)], self.finalAvgWaitingTime)
-        ax_final_avg.set_xlabel('Zeit [Minuten]')
-        secax2 = ax_final_avg.secondary_xaxis('top', functions=(min_to_hour, min_to_hour))
-        secax2.set_xlabel('Zeit [Stunden]')
-        ax_final_avg.set_ylabel('Wartezeit [Minuten]')
-        ax_final_avg.title.set_text('Finale Durchschnittliche Wartezeit')
-        fig.tight_layout()
-        if Conf.showPlots:
-            fig.show()
 
     def apply_decisions(self, decisions: tuple[enums.ElevatorState, enums.ElevatorState, enums.ElevatorState],
                         need_decisions: list[bool, bool, bool]) -> None:
